8e0fc51bc456d93ae0e520a91f03d29a81ab59cf
Viren Ahluwalia <viren3715@gmail.com>
2019-12-06 14:18:16 -0800

    - Made a Match 3Type game for VGP135 Assignment 1. - Added all the required files into the repository.

---
+++ Assets/Scripts/Effects/MyGui.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class MyGui : MonoBehaviour {
+
+	public Vector3 minScale = new Vector3(.5f, .5f, .5f);
+	public float changeDelay = 2.0f;
+	RectTransform rectTransform;
+
+	void Start()
+    {
+		rectTransform = GetComponent<RectTransform>();
+		StartCoroutine(GUI());
+	}
+
+	// Change size down to minScale then back up to max scale
+	IEnumerator GUI() {
+		Vector3 startingScale = rectTransform.localScale;
+		Vector3 currentScale = startingScale;
+		
+		Vector3 changeScale = new Vector3(.01f, .01f, .01f);
+		bool shrink = true;
+
+		while (true) {
+			if (shrink) {
+				currentScale -= changeScale;
+				if (currentScale.x <= minScale.x) {
+					shrink = false;
+				}
+			} else {
+				currentScale += changeScale;
+				if (currentScale.x >= 1) {
+					shrink = true;
+				}
+            }
+			rectTransform.localScale = currentScale;
+			yield return new WaitForSeconds(changeDelay);
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/GUIManager.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class GUIManager : MonoBehaviour {
+    public static GUIManager instance;
+
+    public GameObject gameOverPanel;
+    public Text yourScoreTxt;
+    public Text highScoreTxt;
+
+    public Text scoreTxt;
+    public Text moveCounterTxt;
+
+    private int score;
+    private int moveCounter;
+    private int matchTime;
+    void Awake()
+    {
+        matchTime = 60;
+        moveCounter = matchTime;
+        //moveCounterTxt = moveCounter.ToString();
+        instance = GetComponent<GUIManager>();
+    }
+
+        public int Score
+        {
+        get{return score;}
+
+        set
+        {
+            score = value;
+            scoreTxt.text = score.ToString();
+        }
+    }
+
+    //private void Update()
+    //{
+    //    moveCounter -= (int)Time.deltaTime;
+    //    if(moveCounter < 0)
+    //    {
+    //        GameOver();
+    //    }
+    //}
+
+    public int MoveCounter
+    {
+        get { return moveCounter; }
+
+        set
+        {
+            moveCounter = value;
+            moveCounterTxt.text = moveCounter.ToString();
+        }
+    }
+
+	// Show the game over panel
+	public void GameOver() {
+		GameManager.instance.gameOver = true;
+
+		gameOverPanel.SetActive(true);
+
+		if (score > PlayerPrefs.GetInt("HighScore")) {
+			PlayerPrefs.SetInt("HighScore", score);
+			highScoreTxt.text = "New Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+		} else {
+			highScoreTxt.text = "Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+		}
+
+		yourScoreTxt.text = score.ToString();
+	}
+
+    private IEnumerator WaitForShifting()
+    {
+        yield return new WaitUntil(() => !BoardManager.instance.IsShifting);
+        yield return new WaitForSeconds(2.0f);
+        StartCoroutine(WaitForShifting());
+    }
+
+    private IEnumerator StartCoroutine(int moveCounter)
+    {
+        moveCounter -= (int)Time.deltaTime;
+        if (moveCounter < 0)
+        {
+            GameOver();
+        }
+        yield return new WaitForSeconds(5.0f);
+        StartCoroutine(moveCounter);
+    }
+}
